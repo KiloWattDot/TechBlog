e39730e33bcec6db854b3db02126a445a3b9f1eb
KiloWattDot <74320819+KiloWattDot@users.noreply.github.com>
2022-03-08 13:12:53 -0500

    Finished routes, handlebar pages next

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { User } = require('../../models')
+const { User, Posts, Comments } = require('../../models')
+const withAuth = require('../../utils/auth')
+const bcrypt = require('bcrypt');
 
 //CREATE NEW USER
 router.post('/register', async (res, req) => {
@@ -26,26 +28,82 @@ router.post('/register', async (res, req) => {
     }
 });
 
-
+// User Login
+// Check for valid user email and password along with setting loggedIn as true in session
 router.post('/login', async (req, res) => {
     try {
-        const dbUserdata = await User.findOne({
+        const dbUserData = await User.findOne({
             where: {
                 email: req.body.email,
             },
         });
-        if (!dbUserdata) {
+        // Valid User email check
+        if (!dbUserData) {
             return res.status(400).json({message: 'Incorrect email or password. Please Try again'})
         }
+
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        // Valid Password check
+        if (!validPassword) {
+           return res.status(400).json({message: 'Incorrect email or password. Please Try again'})
+        
+        }
+        
+        // Session attribute loggedIn
         req.session.save(() => { 
-          req.session.loggedIn = true  
+            req.session.loggedIn = true  
+
+            res
+            .status(200)
+            .json({ user: dbUserData, message: 'You are now logged in!' });
+    
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+})
+
+
+// User creates a new Post
+router.post('/post', async (req, res) => {
+    const newPost = await Posts.create({
+        title: req.body.title,
+        description: req.body.description
+    })
+
+    res.status(200).json(newPost)
+})
+
+// User comment post
+router.post('/comment', async (req, res) => {
+    try { 
+        const newComment = await Comments.create({
+            ...req.body, 
+            user_id: req.session.id
 
         });
 
-        res.status(200);
+        res.status(200).json(newComment)
 
-        const validPassword = await bcrypt.
-        
-        
+    } catch (err) {
+        res.status(400).json(err)
     }
 })
+
+
+// User logout and session destroyed
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+
+  module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const { User, Posts, Comments } = require('../models')
+const withAuth = require('../utils/auth')
+const bcrypt = require('bcrypt');
+const { post } = require('./api/user-routes');
+const res = require('express/lib/response');
+
+// Render Homepage, countvisit within session,
+router.get('/', async (res, req) => {
+    req.session.save(() => {
+        req.session.countVisit ? req.session.countVisit++ : req.session.countVisit = 1
+    })
+
+    try {
+        const dbPostData = await Posts.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comments,
+                    attributes: ['description']
+                }
+            ]
+
+        })
+
+        const allPost = dbPostData.map((post) =>
+            post.get({plain: true})    
+        );
+        res.render('homepage', {
+            allPost,
+            loggedIn: req.session.loggedIn,
+            countVisit: req.session.countVisit,
+        
+        })
+    } catch (err) { 
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+
+router.get('register', async (req, res) => {
+    try {
+        res.render('register', {
+            loggedIn: req.session.loggedIn
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+});
+  
+
+module.exports = router;
